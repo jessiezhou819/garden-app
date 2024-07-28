e9a381213d6d3f9fd4bb473ce44016434c270c73
trevdang <trevdang@students.cs.ubc.ca>
2024-07-27 21:34:24 -0700

    Finish Watering implementation for Update, Delete, Insert

--- public/scripts.js
+++ public/scripts.js
@@ -61,6 +61,187 @@ async function fetchAndDisplayHousePeople() {
     });
 }
 
+/**
+ * ALL FUNCTIONALITIES RELATED TO WATERING
+ */
+// Fetches data from joined WateringR1 and WateringR2 table and displays it.
+async function fetchAndDisplayWatering() {
+    const tableElement = document.getElementById('watering');
+    const tableBody = tableElement.querySelector('tbody');
+
+    const response = await fetch('/watering', {
+        method: 'GET'
+    });
+
+    const responseData = await response.json();
+    const wateringContent = responseData.data;
+
+    if (tableBody) {
+        tableBody.innerHTML = '';
+    }
+
+    wateringContent.forEach(watering => {
+        const row = tableBody.insertRow();
+        watering.forEach((field, index) => {
+            const cell = row.insertCell(index);
+            cell.textContent = field;
+        });
+    });
+}
+// Fetches data from WateringR2 and displays it
+async function fetchAndDisplayWateringR2() {
+    const tableElement = document.getElementById('wateringR2');
+    const tableBody = tableElement.querySelector('tbody');
+
+    const response = await fetch('/wateringR2', {
+        method: 'GET'
+    });
+
+    const responseData = await response.json();
+    const wateringR2Content = responseData.data;
+
+    if(tableBody) {
+        tableBody.innerHTML = '';
+    }
+
+    wateringR2Content.forEach(wateringR2 => {
+        const row = tableBody.insertRow();
+        wateringR2.forEach((field, index) => {
+            const cell = row.insertCell(index);
+            cell.textContent = field;
+        });
+    });
+}
+// Fetches data from WateringR1 and displays it
+async function fetchAndDisplayWateringR1() {
+    const tableElement = document.getElementById('wateringR1');
+    const tableBody = tableElement.querySelector('tbody');
+
+    const response = await fetch('/wateringR1', {
+        method: 'GET'
+    });
+
+    const responseData = await response.json();
+    const wateringR1Content = responseData.data;
+
+    if(tableBody) {
+        tableBody.innerHTML = '';
+    }
+
+    wateringR1Content.forEach(wateringR1 => {
+        const row = tableBody.insertRow();
+        wateringR1.forEach((field, index) => {
+            const cell = row.insertCell(index);
+            cell.textContent = field;
+        })
+    })
+}
+// Insert watering entry into WateringR2 and WateringR1 tables
+async function insertWatering(event) {
+    event.preventDefault();
+
+    const wateringId = document.getElementById('insertwateringId').value;
+    const pH = document.getElementById('insertpH').value;
+    const temperature = document.getElementById('inserttemperature').value;
+    const wateringDate = document.getElementById('insertwateringDate').value;
+    const amount = document.getElementById('insertamount').value;
+    const plantId = document.getElementById('insertplantId').value;
+
+    const response = await fetch('/insertWatering', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            wateringId: wateringId,
+            pH: pH,
+            temperature: temperature,
+            wateringDate: wateringDate,
+            amount: amount,
+            plantId: plantId
+        })
+    });
+
+    const responseData = await response.json();
+    const messageElement = document.getElementById('insertWateringResultMsg');
+
+    if (responseData.success) {
+        messageElement.textContent = "Data inserted successfully!";
+        fetchTableData();
+    } else {
+        messageElement.textContent = "Error inserting data!";
+    }
+}
+// Delete watering entry through cascading of WateringR1 table
+async function deleteWatering(event) {
+    event.preventDefault();
+
+    const wateringDate = document.getElementById('deletewateringDate').value;
+    const temperature = document.getElementById('deletetemperature').value;
+    const pH = document.getElementById('deletepH').value;
+
+    const response = await fetch('/deleteWatering', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            wateringDate: wateringDate,
+            temperature: temperature,
+            pH: pH
+        })
+    });
+
+    const responseData = await response.json();
+    const messageElement = document.getElementById('deleteWateringR1ResultMsg');
+
+    if (responseData.success) {
+        messageElement.textContent = "Data deleted successfully!";
+        fetchTableData();
+    } else {
+        messageElement.textContent = "Error deleting data!";
+    }
+}
+// Update WateringR2 entry
+async function updateWateringR2(event) {
+    event.preventDefault();
+
+    const wateringId = document.getElementById('updatewateringId').value;
+    const wateringDate = document.getElementById('updatewateringDate').value;
+    const temperature = document.getElementById('updatetemperature').value;
+    const pH = document.getElementById('updatepH').value;
+    const plantId = document.getElementById('updateplantId').value;
+
+    // console.log(wateringId, wateringDate, temperature, pH, plantId);
+
+    const response = await fetch('/updateWateringR2', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            wateringId: wateringId,
+            wateringDate: wateringDate,
+            temperature: temperature,
+            pH: pH,
+            plantId: plantId
+        })
+    });
+
+    const responseData = await response.json();
+    const messageElement = document.getElementById('updateWateringR2ResultMsg');
+
+    if (responseData.success) {
+        messageElement.textContent = "Data updated successfully!";
+        fetchTableData();
+    } else {
+        messageElement.textContent = "Error updating data!";
+    }
+}
+
+/**
+ * TEMPLATE RELATED FUNCTIONS
+ */
 // Fetches data from the demotable and displays it.
 async function fetchAndDisplayUsers() {
     const tableElement = document.getElementById('demotable');
@@ -109,9 +290,6 @@ async function insertDemotable(event) {
 
     const idValue = document.getElementById('insertId').value;
     const nameValue = document.getElementById('insertName').value;
-
-    console.log(idValue);
-    console.log(nameValue);
 
     const response = await fetch('/insert-demotable', {
         method: 'POST',
@@ -216,9 +394,15 @@ async function countDemotable() {
 // Initializes the webpage functionalities.
 // Add or remove event listeners based on the desired functionalities.
 window.onload = function() {
+    // GARDEN RELATED FUNCTIONS
     checkDbConnection();
     fetchTableData();
     document.getElementById("resetDemotable").addEventListener("click", resetDemotable);
+    document.getElementById("insertWatering").addEventListener("submit", insertWatering);
+    document.getElementById("deleteWateringR1").addEventListener("submit", deleteWatering);
+    document.getElementById("updateWateringR2").addEventListener("submit", updateWateringR2);
+
+    // TEMPLATE RELATED FUNCTIONS
     document.getElementById("insertDemotable").addEventListener("submit", insertDemotable);
     document.getElementById("updataNameDemotable").addEventListener("submit", updateNameDemotable);
     document.getElementById("deleteDemotable").addEventListener("submit", deleteDemotable);
@@ -228,6 +412,12 @@ window.onload = function() {
 // General function to refresh the displayed table data. 
 // You can invoke this after any table-modifying operation to keep consistency.
 function fetchTableData() {
+    // TEMPLATED RELATED FUNCTIONS
     fetchAndDisplayUsers();
+
+    // GARDEN RELATED FUNCTIONS
     fetchAndDisplayHousePeople();
+    fetchAndDisplayWatering();
+    fetchAndDisplayWateringR2();
+    fetchAndDisplayWateringR1();
 }
